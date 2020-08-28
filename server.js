aae78dd85b9ad2cd0d5b7f92c1ef10b8ce315a24
Mukesh A <amukesh.mk@gmail.com>
2020-08-28 11:24:40 +0100

    added @material/core and socket.io deps

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
+import http from 'http';
+import express from 'express';
+import socketio from 'socket.io';
+
 import config from './config';
 import apiRouter from './api';
-import express from 'express';
-const server = express();
 
-server.set('view engine', 'ejs');
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-server.get('/', (req, res) => {
-    res.render('index', {
-        content: 'Hello World!!'
-    });
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-server.use(express.static('public'));
-server.use('/api', apiRouter);
+app.use(express.static('public'));
+app.use('/api', apiRouter);
 
 server.listen(config.port, () => {
     console.info(`App Running on http://localhost:${config.port}`);
+});
+
+io.on('connection', (socket) => {
+    console.info('a user connected');
+
+    io.emit('message', 'Welcome to Chat App!');
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
-ReactDOM.render(
-    React.createElement('h2', null, 'Hello React!'),
-    document.getElementById('root')
-);
+// eslint-disable-next-line no-undef
+const socket = io();
+
+function App() {
+    return (
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
+    );
+}
+
+socket.on('message', message => {
+    ReactDOM.render(<h2>{message}</h2>, document.getElementById('msg'));
+});
+ReactDOM.render(<App />, document.getElementById('root'));
