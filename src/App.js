da38615dfc3285baa2173716f59b41aef6b522b0
Mukesh A <amukesh.mk@gmail.com>
2020-09-13 00:07:27 +0100

    added template singup page and updated react routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
 import Chat from './components/Chat';
+import SignUp from './components/SignUp';
 
 const App = () => (
     <Router>
-        <Route path='/' exact component={Login} />
-        <Route path='/chat' component={Chat} />
+        <Switch>
+            <Route path='/' exact component={Login} />
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/chat' component={Chat} />
+        </Switch>
     </Router>
 );
 
